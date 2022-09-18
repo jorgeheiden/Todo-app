bc4a9dae9cc752b85e979075c949a6732b68a22e
jorge heidenreich <jmjhjorge@gmail.com>
2022-09-18 15:15:16 -0300

    primer commit

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,21 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  ocultarBoton = true
-  constructor() { }
+  tareas = [
+    { tarea: 'tarea 1', estado: false, hover: true },
+    { tarea: 'tarea 2', estado: true, hover: true },
+  ];
+  ocultarBoton = true;
+  
+  constructor() {}
+
+  inputTareas = new FormControl();
+  checkbox = new FormControl()
 
   ngOnInit(): void {
   }
+  inputEnter(tarea: any) {
+    this.tareas.push({ tarea: tarea, estado: false, hover: true });
+    console.log(tarea);
+    //Vacia el input, mediante un reset() del FormContro()
+    this.inputTareas.reset();
+  }
+  estado(tarea:any, evento:any){
 
-  btnEliminar(){
-    this.ocultarBoton = false
+      this.tareas[this.tareas.indexOf(tarea)].estado = evento.target.checked
+
   }
-  ocultarbtn(){
-    this.ocultarBoton = true
+
+  btnEliminar(tarea: any) {
+    console.log(tarea);
+    tarea.hover = false;
   }
+  ocultarbtn() {
+    this.ocultarBoton = true;
+  }
+
+  
 }
