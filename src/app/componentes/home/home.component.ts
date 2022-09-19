d6bfe50d53d71ff18b6d2ce21fc8ade6dd9b7799
jorge heidenreich <jmjhjorge@gmail.com>
2022-09-19 15:02:00 -0300

    Filtro por elementos activos

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -8,38 +8,53 @@ import { FormControl } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   tareas = [
-    { tarea: 'tarea 1', estado: false, hover: true },
-    { tarea: 'tarea 2', estado: true, hover: true },
+    { tarea: 'tarea 1', estado: false, hover: true, ocultar: false },
+    { tarea: 'tarea 2', estado: true, hover: true, ocultar: false },
+    { tarea: 'tarea 3', estado: true, hover: true, ocultar: false },
   ];
   ocultarBoton = true;
-  
+  itemsLeft!: number;
   constructor() {}
 
   inputTareas = new FormControl();
-  checkbox = new FormControl()
+  checkbox = new FormControl();
 
   ngOnInit(): void {
+    this.itemsLeftFunction();
   }
   inputEnter(tarea: any) {
-    this.tareas.push({ tarea: tarea, estado: false, hover: true });
+    this.tareas.push({
+      tarea: tarea,
+      estado: false,
+      hover: true,
+      ocultar: false,
+    });
     console.log(tarea);
     //Vacia el input, mediante un reset() del FormContro()
     this.inputTareas.reset();
+    this.itemsLeftFunction();
   }
-  estado(tarea:any, evento:any){
-
-      this.tareas[this.tareas.indexOf(tarea)].estado = evento.target.checked
-
+  estado(tarea: any, evento: any) {
+    this.tareas[this.tareas.indexOf(tarea)].estado = evento.target.checked;
   }
 
   btnEliminarMostrar(tarea: any) {
-    tarea.hover = false
+    tarea.hover = false;
   }
-  btnEliminarOcultar(tarea:any){
-    tarea.hover = true
+  btnEliminarOcultar(tarea: any) {
+    tarea.hover = true;
   }
-  eliminarTarea(tarea:any){
-    const indice = this.tareas.indexOf(tarea)
-    this.tareas.splice(indice, 1)
+  eliminarTarea(tarea: any) {
+    const indice = this.tareas.indexOf(tarea);
+    this.tareas.splice(indice, 1);
+    this.itemsLeftFunction();
+  }
+  itemsLeftFunction() {
+    this.itemsLeft = this.tareas.length;
+  }
+  active() {
+    this.tareas
+      .filter((elemento) => elemento.estado)
+      .map((elemento) => (elemento.ocultar = true));
   }
 }
